d256bda2200331f577faa66009368ca450210b99
Mike Halagan <mhalagan@nmdp.org>
2018-04-18 11:13:06 -0500

    Fixed issue with IMGT/HLA format changing yet again..

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ test_requirements = [
 
 setup(
     name='pyard',
-    version='0.0.6',
+    version='0.0.7',
     description="ARD reduction for HLA with python",
     long_description=readme + '\n\n' + history,
     author="Michael Halagan",
