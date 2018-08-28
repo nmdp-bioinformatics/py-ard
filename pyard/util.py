a66f4e5693befde6e5346e0cd2891745929574b6
Mike Halagan <mhalagan@nmdp.org>
2018-08-27 19:32:35 -0500

    Addressed issues 8,9 and 11 - v0.0.9 on pypi

--- pyard/util.py
+++ pyard/util.py
@@ -24,7 +24,6 @@
 import os
 import string
 import random as r
-from typing import GenericMeta
 from datetime import datetime, date
 from six import integer_types, iteritems
 import pandas as pd
@@ -122,11 +121,6 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime:
         return deserialize_datetime(data)
-    elif type(klass) == GenericMeta:
-        if klass.__extra__ == list:
-            return _deserialize_list(data, klass.__args__[0])
-        if klass.__extra__ == dict:
-            return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
 
