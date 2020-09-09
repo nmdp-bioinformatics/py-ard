206e6cb062b393d8be7ce1f4917c2bd959742173
rsajulga-nmdp <rsajulga@nmdp.org>
2020-09-09 11:04:10 -0500

    differentiate between reduxing functions in step file

--- tests/steps/redux_allele.py
+++ tests/steps/redux_allele.py
@@ -9,6 +9,11 @@ def step_impl(context, level):
     context.level = level
     context.redux_allele = context.ard.redux(context.allele, level)
 
+@when('reducing on the {level} level (ambiguous)')
+def step_impl(context, level):
+    context.level = level
+    context.redux_allele = context.ard.redux_gl(context.allele, level)
+
 @then('the reduced allele is found to be {redux_allele}')
 def step_impl(context, redux_allele):
     assert_that(context.redux_allele, is_(redux_allele))
