4ecc8dc1dacffd7bd8ce0e62187412735a73659e
pbashyal-nmdp <pbashyal@nmdp.org>
2023-08-02 13:14:25 -0500

    Bump version: 1.0.1 → 1.0.2

--- pyard/__init__.py
+++ pyard/__init__.py
@@ -27,7 +27,7 @@ from .constants import DEFAULT_CACHE_SIZE
 from .misc import get_imgt_db_versions as db_versions
 
 __author__ = """NMDP Bioinformatics"""
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 def init(
