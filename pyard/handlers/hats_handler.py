66dc90ae4dee455bb038e3d9f1a341d15f738b08
Pradeep Bashyal <pbashyal@nmdp.org>
2026-08-05 12:17:19 -0500

    Fix issue with returning other loci alleles with the same HATS

--- pyard/handlers/hats_handler.py
+++ pyard/handlers/hats_handler.py
@@ -17,12 +17,16 @@ class HATSHandler:
 
     def expand_to_hats_alleles(self, alleles_gl: str) -> str:
         alleles = alleles_gl.split("/")
+        locus = alleles[0].split("*")[0]
         hats_alleles = db.find_common_hats_alleles(self.ard.db_connection, alleles)
         # MAC expanded alleles should only be 2 field level alleles
         hats_alleles_2fields = filter(is_2_field_allele, hats_alleles)
+        hats_alleles_same_locus = filter(
+            lambda a: a.split("*")[0] == locus, hats_alleles_2fields
+        )
         return "/".join(
             sorted(
-                hats_alleles_2fields,
+                hats_alleles_same_locus,
                 key=functools.cmp_to_key(self.ard.smart_sort_comparator),
             )
         )
